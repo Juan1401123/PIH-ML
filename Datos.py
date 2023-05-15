b531b1231bb5aa913ed5e9afed02205b20dc7016
juansamadors@gmail.com <juansamadors@gmail.com>
2023-05-15 17:07:04 -0600

    actualizacion2.0

--- Datos.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-
-Ultimate_df= pd.read_csv('../PIH_ML/Final-Datasets/datos-transformados.csv')
-
-ml_matrix_df=pd.read_csv('../PIH_ML/Final-Datasets/ml_matrix.csv')
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-from Datos import Ultimate_df,ml_matrix_df
+import pandas as pd
+Ultimate_df= pd.read_csv('../PIH_ML/Final-Datasets/datos-transformados.csv')
+ml_matrix_df=pd.read_csv('../PIH_ML/Final-Datasets/ml_matrix.csv')
 import ast
 app=FastAPI()
 
